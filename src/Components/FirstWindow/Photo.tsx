bc931d86607c86ab0035c0f58408f752bd18fce0
Ilya Burlakov <72346704+ilyasvtv@users.noreply.github.com>
2023-12-18 12:45:28 +0300

    Update Photo.tsx

--- src/Components/FirstWindow/Photo.tsx
+++ src/Components/FirstWindow/Photo.tsx
@@ -6,16 +6,16 @@ import store from "../../store/store";
 const Photo = () => {
     return (
         <>
-            <InputLabel>{store.photo[0]["name"]}</InputLabel>
-			{store.photo[0]["value"].length > 0 && <img width={100} height={100} src={store.photo[0]["value"]} alt="img" />}
-			<Input
-				placeholder="Input here"
-				name={store.photo[0]["name"]}
-				required={store.photo[0]["required"]}
-				type={store.photo[0]["type"]}
-				variant="outlined"
-				onChange={(e) => store.processFile(e)}
-			/>
+            	<InputLabel>{store.photo[0]["name"]}</InputLabel>
+		{store.photo[0]["value"].length > 0 && <img width={100} height={100} src={store.photo[0]["value"]} alt="img" />}
+		<Input
+			placeholder="Input here"
+			name={store.photo[0]["name"]}
+			required={store.photo[0]["required"]}
+			type={store.photo[0]["type"]}
+			variant="outlined"
+			onChange={(e) => store.processFile(e)}
+		/>
         </>
     )
 }
